54c4b09d7e633b1c5eee4ff69a8a34e821c92b79
Oiyanu <iyanuolatomide@gmail.com>
2023-11-27 20:55:59 -0800

    new commit

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,22 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+#include <stdio.h>
+
+/**
+ * struct listint_s - a singly linked list
+ * @n: int
+ * @next: next node to point
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_s;
+
+int check_cycle(listint_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+#endif
