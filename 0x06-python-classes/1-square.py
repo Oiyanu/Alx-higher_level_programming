813a3663443ff6e6661cca290a85b58adbdf3592
Oiyanu <iyanuolatomide@gmail.com>
2023-12-19 16:56:23 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square module"""
+
+class Square():
+    """A Square class"""
+    def __init__(self, size) -> None:
+        """Initializes an instance attribute(s)"""
+        self.__size = size
