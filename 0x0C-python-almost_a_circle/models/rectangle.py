f9f49f76e908fd7794ccaf9815c7bcf5373475a9
Oiyanu <iyanuolatomide@gmail.com>
2024-01-15 15:23:48 -0800

    update

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python3
+"""Defines Rectangular class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of Rectangle object
+            height (int): The height of Rectangle object
+            x: (int)
+            y: (int)
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    def__str__(self):
+        """Returns the print() and str() representation of the Rectangle"""
+        str_rep = "[{}] ({}) {}/{} - {}/{}"
+        idd, xx, yy, w = self.id, self.__x, self.__y, self.__width
+        h = self.__height
+        return str_rep.format(__class__.__name__, idd, xx, yy, w, h)
+
+    @property
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Set/get the x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Set/get the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.width * self.height
+
+    def display(self):
+        """Print the Rectangle using the `#` character."""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
+    def update(self, *args, **kwargs):
+        """Update the rectangle
+
+        Args:
+            args: parameters for values of class attributes
+            kwargs: dictionary containing new key/value pairs of attributes.
+        """
+        attr = ['id', 'width', 'height', 'x', 'y']
+        if len(args) != 0:
+            for i, arg in enumerate(args):
+                setattr(self, attr[i], arg)
+
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary of rectangle object"""
+        data = {}
+        for attr in self.__dict__:
+            key = attr.lstrip('_').split('_')[-1]
+            value = getattr(self, attr)
+            data[key} = value
+        return data
